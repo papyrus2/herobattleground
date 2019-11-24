4a4ef6b3a3afc28effbcdefce86646f3c521daa0
Iulian Stana <julian.stana@gmail.com>
2019-11-25 01:03:56 +0200

    Create a battle round where the two players fight

--- battleground/battleground_service.py
+++ battleground/battleground_service.py
@@ -1,4 +1,5 @@
 from concurrent import futures
+import random
 
 import grpc
 from protos.python.battleground_pb2_grpc import (
@@ -9,16 +10,70 @@ from characters.helpers import generate_character
 
 class BattlegroundService(BattlegroundServiceServicer):
     """ Battleground Service will handle all the fighting mechanism. """
-    def Fighting(self, request, context):
-        """ Get a generated character base on the request character type.
+    def decide_playing_order(self, first_player, second_player):
+        """ Decide the what player will be first to attack. 
+        The player order will be decided based on the speed and chance
+        """
+        if first_player.speed > second_player.speed:
+            return first_player, second_player
+        elif first_player.speed == second_player.speed:
+            if first_player.chance > second_player.chance:
+                return first_player, second_player
+        return second_player, first_player
 
-        The generated character will have different stats based on the 
-        character attributes configuration.
+    def compute_damage(self, attacker, defencer):
+        """ Compute the damage done by the attacker. """
+        damage = attacker.strength - defencer.defence
+        return damage
+
+    def roll_chance(self, chance):
+        """ See if the player lucky strike. """
+        roll = random.randint(0, 100)
+        return roll <= chance
+
+    def round(self, attacker, defencer):
+        """ Play a round in the battleground, attack and defence try their lucks 
+        and skills. """
+        damage = self.compute_damage(attacker, defencer)
+
+        defence_chance = self.roll_chance(defencer.chance)
+
+        if defence_chance:
+            # no damage done
+            return
+
+        for skill in defencer.skills:
+            if skill.skill_type == character_pb2.DEFENCE:
+                skill_chance = self.roll_chance(skill.chance)
+                if skill_chance:
+                    damage = int(damage / 2)
+
+        defencer.health -= damage
+
+        # TODO: here is a bug the Attack Skill could be executed multiple times
+        for skill in attacker.skills:
+            if skill.skill_type == character_pb2.ATTACK:
+                skill_chance = self.roll_chance(skill_chance)
+                if skill_chance:
+                    self.round(attacker, defencer)
+
+        # chance to defance
+
+    def Fighting(self, request, context):
+        """ The battleground winner will be decided in a legendery fight.
+
+        The defense and attack will be computed here and the battle will 
+        be done over a duration of 20 rounds or until one of the players is dead.
         """
         first_player = request.first_player
         second_player = request.second_player
 
-        return battleground_pb2.BattlegroundResponse(winner=first_player)
+        first_player, second_player = self.decide_playing_order(
+            first_player, second_player)
+
+        self.round(first_player, second_player)
+
+        return battleground_pb2.BattlegroundResponse(winner=second_player)
 
 
 def serve():
